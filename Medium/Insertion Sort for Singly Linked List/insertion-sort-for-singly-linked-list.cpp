358ff3963c47a7f943409409bdb9b19cff31f91d
Pallavikemburu <96564003+Pallavikemburu@users.noreply.github.com>
2024-01-13 21:48:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Link list node
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};*/
+
+class Solution
+{
+    public:
+    Node* insertionSort(struct Node* root_k)
+    {
+        //code here
+        vector<int>v;
+        Node *root=root_k;
+        while(root)
+        {
+            v.push_back(root->data);
+            root=root->next;
+        }
+        sort(v.begin(),v.end());
+        Node *ans=root_k;
+        int i=0;
+        while(ans)
+        {
+            ans->data=v[i];
+            ans=ans->next;
+            i++;
+        }
+        return root_k;
+        
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int n;
+		cin >> n;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < n; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+        Solution ob;
+
+		head = ob.insertionSort(head);
+		printList(head);
+
+		cout << "\n";
+
+
+
+	}
+	return 0;
+}
+// } Driver Code Ends
