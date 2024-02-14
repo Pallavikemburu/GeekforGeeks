55895ebe6f271361a583e64c0cb0065d8802ddbc
Pallavikemburu <96564003+Pallavikemburu@users.noreply.github.com>
2024-02-14 17:48:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Snake and Ladder Problem/snake-and-ladder-problem.cpp
+++ Medium/Snake and Ladder Problem/snake-and-ladder-problem.cpp
@@ -5,85 +5,55 @@
 using namespace std;
 
 // } Driver Code Ends
-// User function Tevplate for C++
+// User function Template for C++
 
 class Solution{
 public:
-    int ans=INT_MAX;
-    int fun(int z,vector<pair<int,int>>&v,int c,vector<int>&dp)
-    {
-        if(z==30)
-        {
-            ans=min(ans,c);
-            return 0;
-        }
-        if(z>30) return 1000;
-        if(dp[z]!=-1) return dp[z];
-        int p=1000;
-        for(int i=1;i<=6;i++)
-        {
-            int t=z+i;
-            if(v[t].first!=0 && v[t].second==0)
-            {
-                continue;
-            }
-            if(v[t].first!=0 && v[t].second!=0)
-            {
-               // i=v[i];
-               int k=v[t].first;
-               p=min(p,1+fun(k,v,c+1,dp));
-            }
-            p=min(p,1+fun(t,v,c+1,dp));
-        }
-        return dp[z]=p;
-        
-        
-        
-        /*
-        int t = z + i;
-        int nextPos = v[t].first;
-        int isLadder = v[t].second;
-        
-        if (nextPos != 0) {
-            if (isLadder) {
-                p = min(p, 1 + fun(nextPos, v, c + 1, dp));
-            } else {
-                p = min(p, 1 + fun(t, v, c + 1, dp));
-            }
-        }  */
-
-        //cout<<p;
-        
-    }
     int minThrow(int n, int a[]){
         // code here
-        
-       // int ans=INT_MAX,c=1;
-        vector<pair<int,int>>v(39,{0,0});
-        vector<int>dp(40,-1);
-       // map<int,int>m;
-       /* for(int i=0;i<n;i=i+2)  // ladder
-        {
-            v[a[i]]={a[i+1],1};
-        }
-        for(int i=n;i<2*n;i=i+2)  // snake
-        {
-            v[a[i]]={a[i+1],0};
-        } */
-        for(int i=0;i<2*n;i=i+2)  // ladder
+       vector<pair<int,int>>v(39,{0,0});
+       vector<int>vis(31,-1);
+       for(int i=0;i<2*n;i=i+2)  
         {
             if(a[i]<a[i+1])  // ladder
             v[a[i]]={a[i+1],1};
-            else  // snake
+            else             // snake
             v[a[i]]={a[i+1],0};
         }
-        /*for(auto i:v) 
+        queue<pair<int,int>>q;
+        q.push({1,0});
+        while(!q.empty())
         {
-            cout<<i.first<<" "<<i.second<<"\n";
-        }   */
-        //cout<<ans;
-       // return 0;
-        return fun(1,v,0,dp);
+            auto z=q.front();
+           // cout<<z.first<<" ";
+            q.pop();
+            for(int i=1;i<=6;i++)
+            {
+                int p=z.first+i;
+               // cout<<p<<" ";
+                if(p==30)
+                    return z.second+1;
+                if(vis[p]==-1)
+                {
+                   // cout<<p<<" ";
+                    if(v[p].first!=0 && v[p].second==0)
+                    {
+                        continue;
+                    }
+                    if(v[p].first!=0 && v[p].second!=0)
+                    {
+                        vis[v[p].first]=1;
+                        q.push({v[p].first,z.second+1});
+                    }
+                    else
+                    {
+                        vis[p]=1;
+                        q.push({p,z.second+1});
+                    }
+                }
+            }
+        }
+        return -1;
     }
 };
 
