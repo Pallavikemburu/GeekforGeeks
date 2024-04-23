6a8f2a1632d56ba57683b59731a4c81954116edf
Pallavikemburu <96564003+Pallavikemburu@users.noreply.github.com>
2024-04-23 21:30:13 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Rohan's Love for Matrix/rohans-love-for-matrix.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int firstElement(int n) {
+        // code here
+        int a=0,b=1,c=0;
+        for(int i=2;i<=n;i++) 
+        {
+            c=a+b;
+            a=b%1000000007;
+            b=c%1000000007;
+        }
+        return b;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.firstElement(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
